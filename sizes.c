c7e948736026b904704e7df23f4503ae26589795
MasahiroKiji <mkiji@coe.edu>
2019-01-25 20:59:55 -0600

    Fix a syntax error

--- sizes.c
+++ sizes.c
@@ -4,7 +4,7 @@ int main() {
 
 printf("The size of int is %lu \n",sizeof(int));
 printf("The size of char is %lu \n",sizeof(char));
-printf("The size of long is %lu \n",sizeof(long);
+printf("The size of long is %lu \n",sizeof(long));
 printf("The size of float is %lu \n",sizeof(float));
 printf("The size of double is %lu \n",sizeof(double));
 printf("The size of unsigned char is %lu \n",sizeof(unsigned char));
